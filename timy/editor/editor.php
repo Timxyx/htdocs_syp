99c1110a0d9691d1833f5c346392d58874407b66
Timxyx <timy.djon@gmail.com>
2022-03-23 21:10:39 +0100

    initial editor

--- timy/editor/editor.php
+++ timy/editor/editor.php
@@ -1,3 +1,12 @@
+<?php 
+    $btn_symbols = ["1234567890","qwertzuiop", "asdfghjkl", "yxcvbnm,.-"];
+    $text = '';
+
+    if(isset($_POST['submit'])){
+        
+        $text = $_POST['text'] . $_POST['submit'];
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,6 +18,25 @@
     <title>simple editor</title>
 </head>
 <body>
-    
+    <div class="container min-w-full flex justify-center">
+        <form action="editor.php" method="POST" class="bg-gray-800 p-5"> 
+            <input type="hidden" name="text" value="<?php echo $text?>"/>
+            <div>  
+                <textarea readonly id="text" name="textArea" cols="35" rows="4"><?php echo $text?></textarea> 	
+                <?php foreach ($btn_symbols as $btn_row): ?>
+                    <div class="keys">
+                        <?php for($i = 0; $i < strlen($btn_row); $i++):?>
+                        <input id="" class="key" type="submit" name="submit" value="<?php echo $btn_row[$i]?>" />
+                <?php endfor;?>
+                </div>
+
+            <?php endforeach;?>
+            <input id="space" class="key" type="submit" name="submit" value="&nbsp;" placeholder="Space"></input>
+            <input id="delete" class="key" type="submit" name="submit" value="&nbsp;" placeholder="Space"></input>
+            <input id="clear" class="key" type="submit" name="submit" value="&nbsp;" placeholder="Space"></input>
+
+            </div> 
+   </div>
+</form> 
 </body>
 </html>
