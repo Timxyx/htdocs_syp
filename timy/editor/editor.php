7599c7ceec8f3981f9372ddfd7749e4c9a9c65e6
Timxyx <62754419+Timxyx@users.noreply.github.com>
2022-03-30 14:35:32 +0200

    finished editor

--- timy/editor/editor.php
+++ timy/editor/editor.php
@@ -1,17 +1,14 @@
 <?php 
     $btn_symbolsLow = ["1234567890","qwertzuiop", "asdfghjkl", "yxcvbnm,.-"];
     $btn_symbolsUp = ["!\§$%&/()=?","`QWERTZUIOP`", "ASDFGHJKL", "YXCVBNM;:_,./"];
-    $btn_symbols = [];
+    $btn_symbols = ["1234567890","qwertzuiop", "asdfghjkl", "yxcvbnm,.-"];
     $text = '';
-    $upper = false;
+    $mode = 'down';
+
     
     
 
     if(isset($_POST['submit'])){
-
-        $upper = filter_var($_POST['upper'], FILTER_VALIDATE_BOOLEAN);
-
-        $btn_symbols = $upper ? $btn_symbolsUp : $btn_symbolsLow;
 
         if($_POST['submit'] == "explode"){
             
@@ -20,12 +17,21 @@
             
             
         }
+        else if($_POST['submit'] == "clear"){
+            $text = "";
+        }
+        else if($_POST['submit'] == "up" || $_POST['submit'] == "down"){
+            $mode = $_POST['submit'];
+        }
+        
         
         else{
             $text = $_POST['text'] . $_POST['submit'];
             
-        } 
-     print_r($upper);   
+        }
+
+
+ 
     }
 ?>
 <!DOCTYPE html>
@@ -42,10 +48,10 @@
     <div class="container min-w-full flex justify-center">
         <form action="editor.php" method="POST" class="bg-gray-800 p-5"> 
             <input type="hidden" name="text" value="<?php echo $text?>"/>
-            <input type="hidden" name="upper" value="<?php echo $upper?>"/>
-            <div>  
-                <textarea readonly id="text" name="textArea" cols="35" rows="4"><?php echo $text?></textarea> 	
-                <?php foreach ($btn_symbols as $btn_row): ?>
+            <input type="hidden" name="mode" value="<?php echo $mode?>"/>
+            <div>
+                <textarea readonly id="text" name="textArea" cols="40" rows="4"><?php echo $text?></textarea> 	
+                <?php foreach ($mode == 'up' ? $btn_symbolsUp : $btn_symbols as $btn_row): ?>
                     <div class="keys">
                         <?php for($i = 0; $i < strlen($btn_row); $i++):?>
                         <input id="" class="key" type="submit" name="submit" value="<?php echo $btn_row[$i]?>" />
@@ -53,11 +59,13 @@
                 </div>
 
             <?php endforeach;?>
-            
-            <input class="labels" id="space" class="key" type="submit" name="submit" value="&nbsp;" placeholder="Space"><label class="labels" for="space"><span class="textSpan" >Space</span></input>
-            <input id="delete" class="key" type="submit" name="submit" value="explode" placeholder="Space"></input><label class="labels" for="space"><span class="textSpan" >Backspace</span></input>
-            <input id="clear" class="key" type="submit" name="upper" value="<?php echo !$upper ?>" placeholder="Upper"></input><label class="labels" for="space"><span class="textSpan" >Caps</span></input>
+            <div class="flex flex-rows w-full space-between">
+                <input class="special bg-white" type="submit" name="submit" value="&nbsp;" placeholder=""><span class="textSpan" >Space</span></input>
+                <input class="special bg-white" type="submit" name="submit" value="explode" placeholder=""></input><span class="textSpan" >Backspace</span></input>
+                <input class="special bg-white" type="submit" name="submit" value="<?php echo $mode == 'up' ? 'down' : 'up' ?>" placeholder=""></input><span class="textSpan" >Caps</span></input>
+                <input class="special bg-white" type="submit" name="clear" value="" placeholder=""></input><span class="textSpan" >Clear</span></input>
 
+            </div>
             </div> 
    </div>
 </form> 
