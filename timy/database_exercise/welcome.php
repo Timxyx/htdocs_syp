35e9f0a39ab260d6e9350c8d44ffadec1261adef
Satalicious <91656114+Satalicious@users.noreply.github.com>
2022-04-24 17:32:14 +0200

    welcome.php progress
    and new functions in dbTools

--- timy/database_exercise/welcome.php
+++ timy/database_exercise/welcome.php
@@ -1,10 +1,15 @@
 <?php
-    session_start();
+    @session_start();
     include("auth.php");
     require_once './dbTools.php';
 
     $history = getHistory($_SESSION['username']);
+    $running = getRunning($_SESSION['username']);
+    
 
+    if(isset($_POST['submit'])) {
+        startTracking($_POST['name'], $_POST['description']);
+    }
 ?>
 
 <!DOCTYPE html>
@@ -38,11 +43,32 @@
                     $diff = gmdate("H:i:s", $diff); 
                 ?>
                 <span class="history--item"><?php echo $diff; ?></span>
-                
             </div>
-        <?php endforeach ?>
-        <div class="active--item">herer <span id="stopwatch">0</span><button onclick="startWatch()">PlayButton</button> </div>
         </div>
+        <?php endforeach ?>
+        <br /><br />
+        <div class="running--container">
+            <?php foreach($running as $item): ?>
+                <div>
+                    <span class="running--item"><?php echo $item[0]; ?></span>
+                    <span class="running--item"><?php echo $item[1]; ?></span>
+                    <span class="running--item"><?php echo $item[2]; ?></span>
+                    <?php
+                        $ts1 = strtotime(str_replace('/', '-', $item[2]));
+                        $ts2 = time();
+                        $diff = abs($ts1 - $ts2);
+                        $diff = gmdate("H:i:s", $diff); 
+                    ?>
+                    <span class="running--item counter"><?php echo $diff; ?></span>
+                </div>
+            <?php endforeach ?>
+        </div>         
+        <div class="active--item">here<span id="stopwatch">0</span><button onclick="startWatch()">PlayButton</button> </div>
+            <form method="POST" action="welcome.php">
+                <input type="text" name="name" />
+                <input type="text" name="description" />
+                <button type="submit" name="submit" >START</button>
+            </form>
         </div>
 
     </div>
